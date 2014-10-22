487dc6917f2894e4a9ba6cdfa56c7f3bdaad5c94
clemente <cuevasclemente@gmail.com>
2014-10-22 13:32:14 -0400

    installer now works

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 cp -R .vim .vimrc ~/
-rm -Rf ~/.vim/bundle/Vundle.vim
-git clone http://www.github.com/gmarik/Vundle.vim ~/.vim/bundle
+rm -Rf ~/.vim/bundle
+mkdir ~/.vim/bundle
+git clone http://www.github.com/gmarik/Vundle.vim ~/.vim/bundle/Vundle.vim
 vim -c PluginInstall
 cp ./.tmux.conf ~/.tmux.conf
 
